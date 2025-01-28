ca3b5d1c8e250fd2c2ce5b15ad9b3620357a543a
Hady Eslam <abdoaslam000@gmail.com>
2025-01-28 21:05:07 +0200

    Add Base Profile And MySQL Profile And Flask Integrations

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='code_profilers',
+    version='0.0.1',
+    author='Hady Eslam',
+    author_email='abdoaslam000@gmail.com',
+    description='Code Profiler Package for profiling your code',
+    long_description='A code profiler package for profiling your code',
+    packages=[],
+    install_requires=[],
+    keywords=['python', 'profilers', 'profiler'],
+)
