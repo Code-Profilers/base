66dc96ae158d54865824c6627221d2c0c269436b
Hady Eslam <abdoaslam000@gmail.com>
2025-01-28 22:43:24 +0200

    Add Some Metadata

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(
     version='1.0.0',
     author='Hady Eslam',
     author_email='abdoaslam000@gmail.com',
-    description='Code Profiler Package for profiling your code',
-    long_description='A code profiler package for profiling your code',
+    description=open('README.md').read(),
+    long_description=open('README.md').read(),
     packages=[],
     install_requires=[],
     keywords=['python', 'profilers', 'profiler'],
+    project_url='Code-Profilers, https://github.com/Code-Profilers/base',
+    project_readme='README.md',
+    url='https://github.com/Code-Profilers/base'
 )
