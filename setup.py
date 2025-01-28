0265549e8aeaf4e93447afcff14d2835bc785c60
Hady Eslam <abdoaslam000@gmail.com>
2025-01-28 21:33:56 +0200

    Add Version And Update README File

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+from code_profilers import version
 
 
 setup(
     name='code_profilers',
-    version='0.0.1',
+    version=version.__version__,
     author='Hady Eslam',
     author_email='abdoaslam000@gmail.com',
     description='Code Profiler Package for profiling your code',
